a6092348ed0b72ab96b2b9d257574721aa7f8b0a
fatmaidris <fatma18650@gmail.com>
2024-01-16 18:04:42 -0800

    done

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    for a in range(1, 101):
+        print = ""
+        if a % 3 == 0:
+            print("Fizz", end=" ")
+        if a % 5 == 0:
+            print("Buzz", end=" ")
+        if a % 15 == 0:
+            print("FizzBuzz", end=" ")
+        else:
+            print("{:d}".format(a), end=" ")
+
