b0370d04cfb2c532d52659b1da0607513aaa1067
fatmaidris <fatma18650@gmail.com>
2024-01-16 17:52:58 -0800

    done

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -10,5 +10,6 @@ def uppercase(str):
     for c in str:
         print(
                 "{:c}"
-                .format(ord(c) if not islower(c) else ord(c) - 32))
+                .format(ord(c) if not islower(c) else ord(c) - 32),
+                end="")
         print()
