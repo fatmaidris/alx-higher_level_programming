a3aa4a904cdcec6d093df5bcb8ecb20263162531
fatmaidris <fatma18650@gmail.com>
2024-02-06 23:51:56 +0000

    done

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Square module."""
+
+
+class Square:
+    """Defines a square."""
+
+    def __init__(self, size):
+	"""Constructure.
+
+	Args:
+	    size: length of the side of the square.
+	"""
+	self.__size = size
