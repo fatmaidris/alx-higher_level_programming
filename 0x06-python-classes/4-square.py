402c92021a6741b7d2ef30f043a5b480938e5bc0
fatmaidris <fatma18650@gmail.com>
2024-02-07 01:10:32 +0000

    done

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""Square module."""
+
+
+class Square:
+    """Defines a square."""
+
+    def __init__(self, size=0):
+        """Constructor.
+
+        Args:
+             size: Length of side of square.
+        """
+        self.size = size
+
+    @property
+    def size(slef):
+        """Property for the length of side of this square.
+
+        Raises:
+            TypeError: if size is not an intege
+            ValueError: if size is less than 0.
+        """
+        return self._size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value,int):
+            raise TypeError('size must be an integer')
+        if value < 0:
+            raise ValueError('size must be >=0')
+        self.__size = value
+
+    def area(self):
+        """Area of square.
+
+        Returns:
+            the size squared.
+        """
+        return self.__size ** 2
---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+"""Square module."""
+
+
+class Square:
+    """Defines a square."""
+
+    def __init__(self, size=0):
+        """Constructor.
+
+        Args:
+             size: Length of side of square.
+        """
+        self.size = size
+
+    @property
+    def size(slef):
+        """Property for the length of side of this square.
+
+        Raises:
+
+            TypeError: if size is not an intege
+            ValueError: if size is less than 0.
+        """
+        return self._size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value,int):
+            raise TypeError('size must be an integer')
+        if value < 0:
+            raise ValueError('size must be >=0')
+        self.__size = value
+
+    def area(self):
+        """Area of square.
+
+        Returns:
+            the size squared.
+        """
+        return self.__size ** 2
+
+    def my_print(self):
+        """Prints this square."""
+        for i in range(self, size):
+            for j in range(self.size):
+                print("#", end="\n" if j is self.size - 1 and i != j else "")
+        print()
